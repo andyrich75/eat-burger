d8df3ed6aa6e7ff4a99baf26645f4192acc6e215
andyrich75 <arichard101@gmail.com>
2019-12-30 09:36:05 -0500

    aded the basic express and server host

---
+++ server.js
@@ -0,0 +1,8 @@
+var express = require("express");
+var PORT = process.env.PORT || 8080;
+var app = express();
+
+
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
