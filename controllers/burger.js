3beb871854f9f7d93cf02f4fb959039030c7e8cc
andyrich75 <arichard101@gmail.com>
2019-12-30 22:41:04 -0500

    updated the controller and handlers for the devoured column

--- controllers/burger.js
+++ controllers/burger.js
@@ -15,9 +15,9 @@ router.get("/", function(req, res) {
 
 router.post("/api/burgers", function(req, res) {
   burger.create([
-    'burger_name'
+    'burger_name', 'devoured'
   ], [
-    req.body.burger_name
+    req.body.burger_name, req.body.devoured
   ], function(result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
@@ -30,7 +30,7 @@ router.put("/api/burgers/:id", function(req, res) {
   console.log("condition", condition);
 
   burger.update({
-    eaten: req.body.eaten
+    devoured: req.body.devoured
   }, condition, function(result) {
     if (result.changedRows == 0) {
       return res.status(404).end();
