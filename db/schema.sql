11136359a2be3702df3d3cdb8050d6f08758466d
andyrich75 <arichard101@gmail.com>
2019-12-30 21:55:29 -0500

    added the auto increment to the sql schema for ID

--- db/schema.sql
+++ db/schema.sql
@@ -4,9 +4,9 @@ CREATE DATABASE sq5huo32n6fm91lo;
 USE sq5huo32n6fm91lo;
 
 CREATE TABLE burgers (
-id INTEGER ,
+id INTEGER auto_increment,
 burger_name VARCHAR(255),
 devoured BOOLEAN,
-createdAt TIMESTAMP NOT NULL,
+createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 PRIMARY KEY(id)
 );
